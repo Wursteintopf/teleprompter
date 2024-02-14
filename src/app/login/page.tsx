9dbfe7f7bb6517e86d6814be19757d0b48f6f17c
Markus Vogel <markus@vogelvlug.de>
2024-02-14 17:39:56 +0100

    Improve login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,33 +1,15 @@
-"use client";
+import { LoginForm } from "@/components/LoginForm/LoginForm";
+import { createClient } from "@/data/supabase/server";
+import { redirect } from "next/navigation";
 
-import { login } from "./actions";
-import { Card } from "@/design-system/components/containers/Card/Card";
-import styles from "./loginForm.module.scss";
-import { Flex } from "@/design-system/components/containers/Flex/Flex";
-import { Button, TextField } from "@mui/material";
+export default async function Page() {
+  const supabase = createClient();
 
-export default function Page() {
-  return (
-    <Card className={styles.loginFormContainer} pt="l" pb="l" pr="l" pl="l">
-      <form action={login}>
-        <Flex direction="column" gap="s">
-          <TextField
-            label="Email"
-            id="email"
-            name="email"
-            type="email"
-            required
-          />
-          <TextField
-            label="Password"
-            id="password"
-            name="password"
-            type="password"
-            required
-          />
-          <Button type="submit">Log in</Button>
-        </Flex>
-      </form>
-    </Card>
-  );
+  const { data } = await supabase.auth.getUser();
+
+  if (data?.user) {
+    redirect("/");
+  }
+
+  return <LoginForm />;
 }
