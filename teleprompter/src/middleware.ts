a962a7b30a1f9106f9378821c84f703041a94994
Markus Vogel <markus@vogelvlug.de>
2024-02-22 19:36:48 +0100

    Remove console log

--- teleprompter/src/middleware.ts
+++ teleprompter/src/middleware.ts
@@ -2,8 +2,6 @@ import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  console.log("running middleware");
-
   let response = NextResponse.next({
     request: {
       headers: request.headers,
